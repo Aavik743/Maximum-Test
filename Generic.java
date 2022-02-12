f3874853bec38516d40a67d90d113db8f4af52f9
unknown <aavik743@gmail.com>
2022-02-13 01:50:53 +0530

    added: ability to add more than 3 parameters

--- Generic.java
+++ Generic.java
@@ -1,7 +1,10 @@
 package javaGenerics;
 
+import java.util.Arrays;
+
 public class Generic<E extends Comparable<E>> {
 	
+//	static ArrayList<E extends comparable<E>> arr = new ArrayList<E extends Comparable<E>>();
 	E x, y, z;
 
 	public Generic(E x, E y, E z) {
@@ -9,35 +12,32 @@ public class Generic<E extends Comparable<E>> {
 		this.x = x;
 		this.y = y;
 		this.z = z;
-	}
+	}	
 
 	public Generic() {
 		
 	}
 
-	public static <E extends Comparable<E>> Comparable  testMaximum (E x, E y, E z) {
-		E max = x;
+	public static <E extends Comparable<E>> Comparable testMaximum (E... values) {
 		
-		if((y).compareTo(max) > 0) {
-			max = y;
-		}
+			Arrays.sort(values);
 		
-		if((z).compareTo(max) > 0) {
-			max = z;
-		}
-		printMax(x,y,z,max);
-		return max;
-		
-	}
+		return values[values.length - 1];
+	}	
 	
-	public static <E extends Comparable<E>> void printMax(E x,E y,E z,E max) {
+	public void printMax(E x,E y,E z,E max) {
 		System.out.printf("max of %s, %s and %s is %s\n",x,y,z,max);
 	}
 	
 	public static void main(String[] args) {
 		
-		Generic.testMaximum("camel", "banana", "apple");
-		Generic.testMaximum(3, 5, 9);
-		Generic.testMaximum(1.33, 1.55, 1.99);
+		System.out.println(Generic.testMaximum("camel", "banana", "apple","ball"));
+		System.out.println(Generic.testMaximum(3, 5, 9,4));
+		System.out.println(Generic.testMaximum(1.33, 1.55, 1.99,1.44));
+		
+		
+		
+		
+		
 	}
 }
